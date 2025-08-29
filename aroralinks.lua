09222de0a917a664879da0ab5b5e2cdff1c7a5de
Browndog7912 <jakevento41704@gmail.com>
2025-08-29 14:51:30 -0700

    Update aroralinks.lua

--- aroralinks.lua
+++ aroralinks.lua
@@ -2,6 +2,10 @@ local link = {
     ["store"] = {
         ["basalt"] = "wget run https://raw.githubusercontent.com/Pyroxenium/Basalt/refs/heads/master/docs/install.lua release latest.lua",
         ["rednetspy"] = "pastebin get s5wXb5fD rednetspy",
+        ["minex"] = "pastebin run N3sufsrA",
+        ["japbapi"] = "wget https://raw.githubusercontent.com/Missooni/CC/refs/heads/main/progressBar/progressBar.lua",
+        ["forms"] = "wget https://raw.githubusercontent.com/Missooni/CC/refs/heads/main/textForms/forms.lua",
+        ["authapi"] = "wget run https://raw.githubusercontent.com/JV-lab-create/CC-APIS/refs/heads/main/installer.minia",
     },
 }
 return link
