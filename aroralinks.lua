23ffede925c8231a87dfcf5f7b34936324bf2fc6
Browndog7912 <jakevento41704@gmail.com>
2025-08-29 16:31:22 -0700

    Update aroralinks.lua

--- aroralinks.lua
+++ aroralinks.lua
@@ -6,6 +6,10 @@ local link = {
         ["japbapi"] = "wget https://raw.githubusercontent.com/Missooni/CC/refs/heads/main/progressBar/progressBar.lua",
         ["forms"] = "wget https://raw.githubusercontent.com/Missooni/CC/refs/heads/main/textForms/forms.lua",
         ["authapi"] = "pastebin run Yi6gq4HR",
+        ["bootscreen.dev"] = "pastebin get NL5ULKf6 bootAnim.lua"
+        ["tts-engine.dev"] = "wget run https://raw.githubusercontent.com/TheAio/tts-engine/main/install.lua"
+        ["packtool"] = "pastebin get g9k6mbuT packtool.lua"
+        ["crashmessage.dev"] = "pastebin get ZkxkftC5 crashMsg.lua"
         ["store"] = "",
     },
 }
