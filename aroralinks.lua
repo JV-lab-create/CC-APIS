cdfdb571e5194c6340feaf77b0f965f26a04a3fa
Browndog7912 <jakevento41704@gmail.com>
2026-06-10 23:20:54 -0700

    Update aroralinks.lua

--- aroralinks.lua
+++ aroralinks.lua
@@ -19,7 +19,9 @@ local link = {
         ["skynet"] = "wget https://raw.githubusercontent.com/osmarks/skynet/master/client.lua /apis/skynet",
         ["telepaint"] = "pastebin get V0gqrj2v",
         ["alongtimeago"] = "wget https://raw.githubusercontent.com/cc-tweaked/treasure-disks/refs/heads/master/data/computercraft/lua/treasure/dan200/alongtimeago/alongtimeago.lua",
-        ["luaide"] = "https://github.com/cc-tweaked/treasure-disks/raw/refs/heads/master/data/computercraft/lua/treasure/gravity_score/luaide/luaide.lua",
+        ["luaide"] = "wget https://github.com/cc-tweaked/treasure-disks/raw/refs/heads/master/data/computercraft/lua/treasure/gravity_score/luaide/luaide.lua",
+        ["battleship"] = "wget https://raw.githubusercontent.com/cc-tweaked/treasure-disks/refs/heads/master/data/computercraft/lua/treasure/gopher_atl/battleship/battleship.lua",
+        ["npaintpro"] = "wget https://github.com/cc-tweaked/treasure-disks/blob/master/data/computercraft/lua/treasure/nitrogenfingers/npaintpro/npaintpro.lua",
     },
 }
 return link
