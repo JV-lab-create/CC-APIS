6f9774f6fe86807f3da0d4ff1196adad196d5362
Browndog7912 <164926568+Browndog7912@users.noreply.github.com>
2026-08-04 23:57:22 -0700

    Update packages.lua

--- apis/auroraget/packages.lua
+++ apis/auroraget/packages.lua
@@ -7,7 +7,7 @@ local link = {
         ["progressBarApi"] = { type = "file", fileURL = "https://raw.githubusercontent.com/Missooni/CC/refs/heads/main/progressBar/progressBar.lua", filePath = "/apis/progbarapi.lua", name = "progress.bar.api"},
         ["forms"] = { type = "file", fileURL = "https://raw.githubusercontent.com/Missooni/CC/refs/heads/main/textForms/forms.lua", filePath = "/apis/forms.lua", name = "HTML.forms.api"}, 
         ["authapi"] = { type = "command", command = "pastebin run Yi6gq4HR" },
-        ["bootscreen.dev"] = "pastebin get NL5ULKf6 bootAnim.lua", 
+        ["bootscreen.dev"] = { type = "command", command = "pastebin get NL5ULKf6 bootAnim.lua" },
         ["tts-engine.dev"] = { type = "command", command = "wget run https://raw.githubusercontent.com/TheAio/tts-engine/main/install.lua" },
         ["packtool"] = { type = "pastebin", id = "g9k6mbuT", filePath = "/program/packtool.lua", name = "packtool"},
         ["crashmessage.dev"] = { type = "command", command = "wget run pastebin get ZkxkftC5 crashMsg.lua release latest.lua" },
@@ -16,8 +16,8 @@ local link = {
         ["TeleClass"] = { type = "pastebin", id = "t2TvSiSU", filePath = "/apis/TeleClass.lua", name = "TeleClass"},
         ["TeleEvents"] = { type = "pastebin", id = "KA2dK07y", filePath = "/apis/TeleEvents.lua", name = "TeleEvents"},
         ["bpi"] = { type = "pastebin", id = "sSSyDAFm", filePath = "/apis/bpi.lua", name = "bpi"},
-        ["DiscordHook"] = { type = "file", fileURL = "https://raw.githubusercontent.com/Wendelstein7/DiscordHook-CC/master/DiscordHook.lua", filePath = "/apis/skynet.lua", name = "Discord Hook"} 
-        ["skynet"] = { type = "file", fileURL = "https://raw.githubusercontent.com/osmarks/skynet/master/client.lua", filePath = "/apis/skynet.lua", name = "Lua IDE"}
+        ["DiscordHook"] = { type = "file", fileURL = "https://raw.githubusercontent.com/Wendelstein7/DiscordHook-CC/master/DiscordHook.lua", filePath = "/apis/discordhook.lua", name = "Discord Hook"},
+        ["skynet"] = { type = "file", fileURL = "https://raw.githubusercontent.com/osmarks/skynet/master/client.lua", filePath = "/apis/skynet.lua", name = "Skynet"},
         ["telepaint"] = { type = "pastebin", id = "V0gqrj2v", filePath = "/program/telepaint/telepaint.lua", name = "telepaint"},
         ["alongtimeago"] = { type = "command", fileURL = "https://raw.githubusercontent.com/cc-tweaked/treasure-disks/refs/heads/master/data/computercraft/lua/treasure/dan200/alongtimeago/alongtimeago.lua"},
         ["luaide"] = { type = "file", fileURL = "https://github.com/cc-tweaked/treasure-disks/raw/refs/heads/master/data/computercraft/lua/treasure/gravity_score/luaide/luaide.lua", filePath = "/program/luaide/luaide.lua", name = "Lua IDE"},
